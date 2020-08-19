52a1d27c27812187588372c9ed0585a0d7074b1e
luc1dLife <luc1d_life@protonmail.com>
2020-08-19 00:25:32 -0600

    Completed Initial Application

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - [x] [Prompt for case, numeric and special characters options](https://github.com/luc1dLife/PasswordGenerator/issues/3)
 - [x] [Validate user input](https://github.com/luc1dLife/PasswordGenerator/issues/4)
 - [x] [Resolve bugs and integrate final changes](https://github.com/luc1dLife/PasswordGenerator/issues/5)
+- [x] [Add signature style](https://github.com/luc1dLife/PasswordGenerator/issues/6)
 <h6><p align="right">[ Click the below image to view the <a href="https://luc1dlife.github.io/PasswordGenerator/">webapplication</a> ]</p></h6>
 <a href="https://luc1dlife.github.io/PasswordGenerator/">
   <img src="https://raw.githubusercontent.com/luc1dLife/PasswordGenerator/master/assets/img/Preview.png" alt="Web App Screeshot">
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,31 @@
 // Set newPassword's Preferences
-
+function userPreferences() {
+  confirm("Welcome to Password Generator.");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  userPreferences();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 // Randomize the string using Math.random
 function generatePassword(length = 12) {
-  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-  var numbers = '0123456789';
-  var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var symbols = "!\"#$%&'()*+,-./:;<=>?@^[\\]^_`{|}~";
   var all = uppercase + lowercase + numbers + symbols;
-  var password = '';
+  var password = "";
   for (var index = 0; index < length; index++) {
       var character = Math.floor(Math.random() * all.length);
       password += all.substring(character, character + 1);
