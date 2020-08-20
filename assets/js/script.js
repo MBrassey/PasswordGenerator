81331f23b133b16a69192249e739567cfef68ddc
luc1dLife <luc1d_life@protonmail.com>
2020-08-19 20:45:35 -0600

    Completed this feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
+// User Preferences Object
+var preferencesObj = {
+  length: 8, // Integer Between [8-128]
+  case: true, // true [UPPERCASE], false [lowercase]
+  numeric: true, // true [On], false [Off]
+  special: true, // true [On], false [Off]
+  reset: function () {
+      (this.length = 8), (this.case = true), (this.numeric = true), (this.special = true);
+  },
+};
+
 // Set newPassword's Preferences
 function userPreferences() {
-  confirm("Welcome to Password Generator.");
+  var passwordLength = "";
+
+  // Validate user input
+  while (passwordLength === "" || passwordLength === null) {
+      var passwordLength = prompt("How long should the password be?");
+      console.log(passwordLength);
+      if (passwordLength >= 8 && passwordLength <= 128) { // Ensure length is within range
+          preferencesObj.length = passwordLength;
+          console.log(preferencesObj.length);
+          return preferencesObj.length;
+      } else {
+          window.alert("Enter a length between 8 - 128!");
+      }
+  }
 }
 
 // Get references to the #generate element
@@ -15,11 +39,8 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // Randomize the string using Math.random
-function generatePassword(length = 12) {
+function generatePassword(length = preferencesObj.length) {
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "0123456789";
@@ -33,3 +54,6 @@ function generatePassword(length = 12) {
   console.log(password);
   return password;
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
